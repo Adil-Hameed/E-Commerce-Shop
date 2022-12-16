4f3da05acfc1bd9c5b263a68e957d4df48f619c6
engradilhameed <adilhameed200@gmail.com>
2022-12-16 12:08:02 +0500

    first push

---
+++ frontend/build/precache-manifest.93c54a27bf66c2ddb002fd87dd11e59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534f8f211711b7884eb4689ecad85a12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd81cb57b644854ed1e",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd81cb57b644854ed1e",
+    "url": "/static/js/main.37f1c459.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
